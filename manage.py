ff400cd6c98e20ec4aa9e198aa64a49b9d9ef968
Saurabh Maurya <saurabhmaurya725682@gmail.com>
2025-03-30 21:09:12 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@
 import os
 import sys
 
+from django.core.wsgi import get_wsgi_application
 
 def main():
     """Run administrative tasks."""
@@ -20,3 +21,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+app = get_wsgi_application()
