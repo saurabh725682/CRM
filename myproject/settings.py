c422ab7b0a4e9e7e4211e92f6a4d1949b8832d64
Saurabh Maurya <saurabhmaurya725682@gmail.com>
2025-03-30 16:23:24 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -17,8 +17,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['.vercel.app']
-
+ALLOWED_HOSTS = ["your-vercel-project-name.vercel.app", "127.0.0.1"]
 
 # Application definition
 
