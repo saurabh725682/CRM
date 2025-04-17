362becc77b85a00b4130dd565646bb54e75363b4
Saurabh Maurya <saurabhmaurya725682@gmail.com>
2025-04-17 16:21:36 +0530

    Update models.py

--- crm/models.py
+++ crm/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Record(models.Model):
-    
     creation_date = models.DateTimeField(auto_now_add=True)    
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -13,7 +12,6 @@ class Record(models.Model):
     city = models.CharField(max_length=300)
     state = models.CharField(max_length=100)
     
-    def __str__(self):
-        
+    def __str__(self):      
         return self.first_name + "  " + self.last_name
     
