acbb452e477380893d622acfffb1cbfc62c3691f
Saurabh Maurya <saurabhmaurya725682@gmail.com>
2025-04-17 16:20:57 +0530

    Update forms.py

--- crm/forms.py
+++ crm/forms.py
@@ -5,21 +5,18 @@ from django import forms
 from .models import Record
 
 # Register/Create a user
-
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2']
         
 # Login a user --
-
 class LoginForm(AuthenticationForm):
     
     username = forms.CharField(widget=TextInput())
     password = forms.CharField(widget=PasswordInput())
     
 # Create a record --
-
 class CreateRecordForm(forms.ModelForm):
     
     class Meta:        
@@ -27,9 +24,7 @@ class CreateRecordForm(forms.ModelForm):
         fields = ['first_name', 'last_name', 'email', 'phone', 'address', 'city', 'state']
         
 # Update a record --
-
 class UpdateRecordForm(forms.ModelForm):
-    
     class Meta:       
         model = Record
         fields = ['first_name', 'last_name', 'email', 'phone', 'address', 'city', 'state']
